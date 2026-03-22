2c5c7abb0bf91cc4a4bdc0a2599c862f8aa4b779
Harshit Raj <harshitraj1236@gmail.com>
2026-03-22 22:14:29 +0530

    Add files via upload

---
+++ fetch_error.swift
@@ -0,0 +1,75 @@
+//
+//  fetch_error.swift
+//  trainning
+//
+//  Created by Harshit Raj on 04/02/26.
+//
+//
+import SwiftUI
+struct PhotoError:Codable,Identifiable{
+    let id: String
+    let author: String
+    let download_url: String
+}
+struct FetchError: View {
+    let columns=[GridItem(.flexible())]
+    @State private var photos:[PhotoError]=[]
+    @State private var showAlert:Bool=false
+    @State private var errorMessage:String=""
+    @MainActor
+    func fetchData() async {
+        guard let url=URL(string:"https://picsum.photos/bad_url")
+        else{
+            return
+        }
+        do{
+            let (data,_)=try await URLSession.shared.data(from:url)
+            self.photos=try JSONDecoder().decode([PhotoError].self,from:data)
+        }
+        catch{
+            errorMessage="Could not load images. Please check your internet."
+            showAlert=true
+        }
+    }
+    var body: some View {
+        ScrollView{
+            LazyVGrid(columns:columns){
+                ForEach(photos){photo in
+                    VStack{
+                        AsyncImage(url: URL(string:photo.download_url)){image in
+                            image
+                                .resizable()
+                                .scaledToFit()
+                        }placeholder: {
+                            ProgressView()
+                        }
+                        .frame(height: 100)
+                        .clipped()
+                        Text(photo.author)
+                            .font(.system(size:20,weight: .semibold,design: .serif))
+                            .foregroundColor(.brown)
+                    }
+                }
+            }
+        }
+        .task {
+            await fetchData()
+        }
+        .alert("Something Wrong!", isPresented: $showAlert){
+            Button("Cancel",role: .cancel){}
+            Button {
+                Task{
+                    await fetchData()
+                }
+            } label: {
+                Text("Retry")
+            }
+
+        }message:{
+            Text(errorMessage)
+        }
+    }
+}
+#Preview {
+    FetchError()
+}
