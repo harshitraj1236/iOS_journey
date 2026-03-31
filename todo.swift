1b308ce4196ea30268eef128cbe207128eafa579
Harshit Raj <harshitraj1236@gmail.com>
2026-03-31 22:57:37 +0530

    Refactor TodoHero view and add new task functionality

--- todo.swift
+++ todo.swift
@@ -1,27 +1,44 @@
-//
-//  todo.swift
-//  trainning
-//
-//  Created by Harshit Raj on 26/12/25.
-//
 import SwiftUI
 struct TodoItem: Identifiable {
     let id = UUID()
     let title : String
     var isCompleted : Bool
 }
+
 struct TodoHero: View {
+    
     @State private var todos = [
-            TodoItem(title: "Learn Swift", isCompleted: false),
-            TodoItem(title: "Build an App", isCompleted: false),
-            TodoItem(title: "Drink Water", isCompleted: true)
-        ]
+        TodoItem(title: "Learn Swift", isCompleted: false),
+        TodoItem(title: "Build an App", isCompleted: false),
+        TodoItem(title: "Drink Water", isCompleted: true)
+    ]
+    
+    @State private var newTodo:String = ""
+    
     var body:some View{
-        List{
-            ForEach($todos){ $todo in
+        VStack(alignment: .center,spacing: 14){
+            HStack(spacing: 20){
+                TextField("Add New Task", text: $newTodo)
+                    .border(Color.blue)
+                    .textFieldStyle(.roundedBorder)
+                    .font(Font.system(size: 20, weight: .semibold, design: .serif))
+                
+                Button("Add"){
+                    let trimmedTitle = newTodo.trimmingCharacters(in: .whitespaces)
+                    guard !trimmedTitle.isEmpty
+                    else{
+                        return
+                    }
+                    todos.append(TodoItem(title: trimmedTitle, isCompleted: false))
+                    newTodo = ""
+                }
+            }
+            .padding()
+            
+            List($todos){$todo in
                 HStack{
-                    Image(systemName:todo.isCompleted ? "checkmark.circle.fill":"circle")
-                        .foregroundColor(todo.isCompleted ? .green : .red)
+                    Image(systemName: todo.isCompleted ? "checkmark.circle.fill" : "circle")
+                        .foregroundColor(todo.isCompleted ? .green : .gray)
                     Text(todo.title)
                         .strikethrough(todo.isCompleted)
                 }
@@ -31,9 +48,8 @@ struct TodoHero: View {
                 }
             }
         }
-        .padding()
     }
 }
-#Preview {
-    TodoHero()
-}
+    #Preview {
+        TodoHero()
+    }
